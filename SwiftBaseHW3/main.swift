a3d84c1b5256085386bed07767bbf1fd83c557ec
Vladimir <p.vladimir.m@yandex.ru>
2020-12-20 19:53:53 +0300

    Car structure without methods

--- SwiftBaseHW3/main.swift
+++ SwiftBaseHW3/main.swift
@@ -7,5 +7,38 @@
 
 import Foundation
 
-print("Hello, World!")
+enum EngineActions {
+    case start, stop
+}
+
+enum WindowsActions {
+    case open, close
+}
+
+enum LoadingActions {
+    case add(weight: Double)
+    case remove(weight: Double)
+}
+
+//The only structure because there are no differences between SportCar and TrunkCar in the home work description.
+struct Car {
+    let model: String
+    let year: Int
+    let trunkVolume: Double
+    var isEngineRunning: Bool
+    var isWindowsOpened: Bool
+    var currentLoad: Double
+    
+    mutating func actionToEngine(action: EngineActions) {
+        
+    }
+    
+    mutating func actionToWindows(action: WindowsActions) {
+        
+    }
+    
+    mutating func actionToTrunk(action: LoadingActions) {
+        
+    }
+}
 
